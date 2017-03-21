33536b24c65ece9a470d96e3bc3112081800e5bf
GuillermoMarcillo <marcillo@iastate.edu>
2017-03-21 11:04:13 -0500

    A project
    Uploading this stuff

---
+++ R/rye_biomV1.R
@@ -0,0 +1,65 @@
+# The ryebiom_v1 function
+#
+# This function reads weather inputs, initialize a list of parameters, and
+# simulates cover crop growth at a daily time-step. Biomass simulation outputs for a single
+# site are produced with a single run over single or multiple growth seasons. As such,
+# a simulation begins and stops over seeding and termination in consecutive years of
+# a cover crop (i.e., fall seeding and next-year's spring termination)
+
+#The simple model follows a resource capture and transformation under non-limiting conditions,
+# and therefore, no stresses of any kind are considered for now.
+# No major phenological description is included yet, with development being driven by
+# simple thermal time accumulation and leaf expansion routines.
+#
+# It is meant to work on a dataframe with weather records processed by the weather_rb function.
+# Parameters refer to a list of physiologically meaningfulcoefficients driving the dynamic accumulation
+# of biomass.
+
+
+rye_biomV1 <- function(param, weather, sdate, tdate){
+  Tbase = param["Tbase"]                            #Define paramaters needed
+  RUE= param["RUE"]
+  K= param["K"]
+  alpha= param["alpha"]
+  LAImax= param["LAImax"]
+  TTM= param["TTM"]
+  TTL= param["TTL"]
+
+  TT= rep(NA, tdate)                                #Define state variables
+  B= rep(NA, tdate)
+  LAI= rep(NA, tdate)
+
+  TT[sdate]= 0                                     #Initialize state variables before simulation
+  LAI[sdate]= 0.01
+  B[sdate]= 1
+
+  for (day in sdate:(tdate-1)){                                        #Update state variables
+    dTT= max ((weather$maxt[day] + weather$mint[day])/2 - Tbase, 0)
+    if (TT[day] <= TTL){
+      dLAI= alpha * dTT * LAI[day] * max(LAImax - LAI[day], 0)
+    }else{
+      dLAI= 0
+    }
+    if(TT[day] <= TTM){
+      dB= RUE * (1 - exp(-K * LAI[day])) * weather$radn[day]
+    }else{
+      dB= 0
+    }
+
+    TT[day + 1]= TT[day] + dTT
+    LAI[day + 1]= LAI[day] + dLAI
+    B[day + 1]= B[day] + dB
+  }
+
+  return(data.frame( day= sdate:tdate,
+                     TT= TT[sdate:tdate],
+                     LAI= LAI[sdate:tdate],
+                     B= B[sdate:tdate]))
+}
+
+
+
+
+
+
+
---
+++ R/weather_rb.R
@@ -0,0 +1,23 @@
+# The Weather_rb function
+#
+# This function builds a weather input array in a format such that a rye
+# cover crop simulation can be run over a 2-year typical growth season,
+# that is, from seeding the cover crop in the Fall to termination in the
+# spring of the subsequent year.
+#
+# It is meant to work on a .csv, or .txt file with weather records, as
+# usually downloaded and processed in the format needed by the Agricultural
+# production simulator (APSIM v.7.8)
+
+weather_rb<- function(x){
+  weather= data.frame(x)
+  colnames(weather)= c("year", "day", "radn", "maxt", "mint", "rain")
+  return(weather)
+}
+
+#not print:
+#ames2016<- read.table("C:/Users/Guillermo/Dropbox/RyeBiom/Weather_files/Weather_Ames_2016.txt",
+#                      skip=11)
+
+ames2<- ames2016
+rownames(ames2)<- ames2016$V2
